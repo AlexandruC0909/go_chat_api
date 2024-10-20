4bc691c1fc3120227d233c0e3ba6fe746b03a6b3
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2024-10-20 17:45:00 +0200

    rmv comments

--- main.go
+++ main.go
@@ -14,20 +14,18 @@ var addr = flag.String("addr", ":8085", "http server address")
 func main() {
 	flag.Parse()
 
-	// Create log file
 	logFile, err := os.OpenFile("server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Failed to open log file:", err)
 	}
 	defer logFile.Close()
 
-	// Set log output
 	log.SetOutput(logFile)
 
 	wsServer := NewWebsocketServer()
 	go func() {
 		log.Println("Starting WebSocket server...")
-		wsServer.Run() // No error handling needed since Run does not return any value
+		wsServer.Run()
 	}()
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
@@ -35,14 +33,12 @@ func main() {
 		ServeWs(wsServer, w, r)
 	})
 
-	// Log server start
 	log.Println("Starting HTTP server on", *addr)
 	err = http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("HTTP server failed to start:", err)
 	}
 
-	// Handle termination gracefully
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
